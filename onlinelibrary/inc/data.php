f88240bad72c1e1270950ed33fbd3cda73fa6a9d
ZariaAdjoe <50431131+ZariaAdjoe@users.noreply.github.com>
2019-06-03 18:22:47 -0400

    Add files via upload

---
+++ onlinelibrary/inc/data.php
@@ -0,0 +1,162 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Everything,Everything",
+	"img" => "img/media/everything.jpg",
+    "genre" => "Romance Novel",
+    "format" => "Hardcover",
+    "year" => 2015,
+    "category" => "Books",
+    "authors" => [
+        "Nicola Yoon"
+    ],
+    "publisher" => "Delacorte Books",
+    "isbn" => '978504132340'
+];
+$catalog[102] = [
+    "title" => "Eleanor and Park",
+    "img" => "img/media/elanor.jpg",
+    "genre" => "Romance Novel",
+    "format" => "Hardback",
+    "year" => 2012,
+    "category" => "Books",
+    "authors" => [
+        "Rainbow Rowell"
+    ],
+    "publisher" => "St. Martin's Press",
+    "isbn" => '1250012570'
+];
+$catalog[103] = [
+    "title" => "The Longest Ride",
+    "img" => "img/media/longestride.jpg",
+    "genre" => "Romance Novel",
+    "format" => "Paperback",
+    "year" => 2013,
+    "category" => "Books",
+    "authors" => [
+        "Nicholas Sparks"
+    ],
+    "publisher" => "Grand Central Publishing",
+    "isbn" => '9781455584727'
+];
+$catalog[104] = [
+    "title" => "The Choice",
+    "img" => "img/media/choice.jpg",
+    "genre" => "Romance Novel",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Nicholas Sparks"
+    ],
+    "publisher" => "Grand Central Publishing",
+    "isbn" => '9781593081157'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Me Before You",
+    "img" => "img/media/me.jpg",
+    "genre" => "Romantic Drama",
+    "format" => "DVD/Streaming",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "Thea Sharrock",
+    "writers" => [
+        "Jojo Moyers",
+    ],
+    "stars" => [
+        "Emilia Clarke",
+        "Sam Clifton",
+    ]
+];
+$catalog[202] = [
+    "title" => "Love Actually",
+    "img" => "img/media/love.jpg",
+    "genre" => "Roamnce",
+    "format" => "DVD/Streaming",
+    "year" => 2003,
+    "category" => "Movies",
+    "director" => "Richard Curtis",
+    "writers" => [
+        "Richard Curtis",
+				"Christopher Nolan"
+    ],
+    "stars" => [
+			"Keira Knightley",
+			"Hugh Grant",
+			"Liam Neeson"
+    ]
+];
+$catalog[203] = [
+    "title" => "One Day",
+    "img" => "img/media/day.jpg",
+    "genre" => "Romance",
+    "format" => "DVD",
+    "year" => 2011,
+    "category" => "Movies",
+    "director" => "Lone Scherfig",
+    "writers" => [
+        "David Nicholls",
+    ],
+    "stars" => [
+			"Anne Hathaway",
+			"Jim Sturgress",
+    ]
+];
+$catalog[204] = [
+    "title" => "The Lion King",
+		"img" => "img/media/lion.jpg",
+	  "genre" => "Drama",
+	  "format" => "DVD/Streaming",
+	  "year" => 1994,
+	  "category" => "Movies",
+	  "director" => "Rob Minkoff",
+	  "writers" => [
+	      "Chris Sanders"
+
+	  ],
+	  "stars" => [
+	      "James Earl Jones",
+	      "Nathan Lane",
+	      "Jeremy Irons"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "2014 Forest Hills Drive",
+	"img" => "img/media/jcole.jpg",
+	"genre" => "Hip Hop",
+	"format" => "CD",
+	"year" => 2014,
+	"category" => "Music",
+	"artist" => "J.Cole"
+];
+
+$catalog[302] = [
+	"title" => " Thiller",
+	"img" => "img/media/thriller.jpg",
+	"genre" => "Pop",
+	"format" => "Streaming Services",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Joji ft. Clams Casino"
+];
+$catalog[303] = [
+	"title" => "Graduation",
+	"img" => "img/media/grad.jpg",
+	"genre" => "Hip Hop",
+	"format" => "CD",
+	"year" => 2007,
+	"category" => "Music",
+	"artist" => "Kanye West"
+];
+$catalog[304] = [
+	"title" => "Golden",
+	"img" => "img/media/golden.jpg",
+	"genre" => "R&B",
+	"format" => "CD",
+	"year" => 2004,
+	"category" => "Music",
+	"artist" => "Jill Scott"
+];
